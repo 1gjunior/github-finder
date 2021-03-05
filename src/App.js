d07ba74e05c2b5e32519747094b4d71772c3ab7a
Gilberto Junior <junior.siilva182@gmail.com>
2021-03-05 16:22:55 -0300

    added Users component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <UserItem />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
