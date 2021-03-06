c43b440b674524f658f3e37758b5a0f7b52b7a58
Gilberto Junior <junior.siilva182@gmail.com>
2021-03-05 23:17:07 -0300

    added the navbar links

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ icon, title }) => {
   return (
@@ -7,6 +8,10 @@ const Navbar = ({ icon, title }) => {
       <h1>
         <i className={icon} /> {title}
       </h1>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </ul>
     </nav>
   );
 };
