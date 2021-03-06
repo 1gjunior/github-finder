ea7909e19e06664e2ea0306044dfda9398eadbd7
Gilberto Junior <junior.siilva182@gmail.com>
2021-03-05 22:52:01 -0300

    added the Alert component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import axios from 'axios';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 
 class App extends React.Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   // Search Github Users
@@ -25,6 +27,13 @@ class App extends React.Component {
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
+  //Set alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
+
   render() {
     const { users, loading } = this.state;
 
@@ -32,10 +41,12 @@ class App extends React.Component {
       <div className="App">
         <Navbar />
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -10,6 +10,7 @@ class Search extends Component {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
     showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
   };
 
   onChange = (e) => {
@@ -18,8 +19,12 @@ class Search extends Component {
 
   onSubmit = (e) => {
     e.preventDefault();
-    this.props.searchUsers(this.state.text);
-    this.setState({ text: '' });
+    if (this.state.text === '') {
+      this.props.setAlert('Please enter shomething', 'light');
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: '' });
+    }
   };
 
   render() {
