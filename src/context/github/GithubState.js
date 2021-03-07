8ee26725b9c39a62de3eec141df5585a24ae5ee6
Gilberto Junior <junior.siilva182@gmail.com>
2021-03-07 19:45:57 -0300

    added initial ContextApi boilerplate

---
+++ src/context/github/GithubState.js
@@ -0,0 +1,47 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import GithubContext from './githubContext';
+import GithubReducer from './githubReducer';
+import {
+  SEARCH_USERS,
+  SET_LOADING,
+  CLEAR_USERS,
+  GET_USER,
+  GET_REPOS,
+} from '../types';
+
+const GithubState = (props) => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+  // Search users
+
+  // Get user
+
+  // Get repos
+
+  // Clear users
+
+  // Set loading
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.user,
+        user: state.user,
+        repos: state.repos,
+        loading: state.loading,
+      }}
+    >
+      {props.children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubState;
