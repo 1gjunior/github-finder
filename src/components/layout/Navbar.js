2ce023e9840585873317811269d9d41484bf515e
Gilberto Junior <junior.siilva182@gmail.com>
2021-03-08 15:26:53 -0300

    added Link to Home in logo page

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -6,7 +6,9 @@ const Navbar = ({ icon, title }) => {
   return (
     <nav className="navbar bg-primary">
       <h1>
-        <i className={icon} /> {title}
+        <Link to="/">
+          <i className={icon} /> {title}
+        </Link>
       </h1>
       <ul>
         <Link to="/">Home</Link>
